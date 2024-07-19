891c907d6a28260bd78bdf5511ea3f18a8b2234b
Kwed <159473699+Kwed1@users.noreply.github.com>
2024-07-19 16:12:03 +0400

    fix

--- client/web/src/components/ui/boostpage/boostTable.tsx
+++ client/web/src/components/ui/boostpage/boostTable.tsx
@@ -65,7 +65,7 @@ const BoostTable = () => {
     return (
         <div className='flex flex-col gap-3 h-[400px] text-white'>
             {upgrades.map((upgrade, index) => (
-                <div className='bg-black h-[100px] rounded-2xl px-4 py-1 flex justify-between' key={index}>
+                <div className='bg-black rounded-2xl px-4 py-2 flex justify-between' key={index}>
                     <div className='text-left'>
                         <p className='text-lg'>{camelNames[index % camelNames.length]}</p>
                         <p className='text-[10px] opacity-80'>{camelDescr[index % camelDescr.length]}</p>
@@ -82,7 +82,7 @@ const BoostTable = () => {
                         </div>
                     </div>
                     <div className='relative'>
-                        <img src={camelImages[index % camelImages.length]} alt="" className='w-[80px] z-10 relative p-2' />
+                        <img src={camelImages[index % camelImages.length]} alt="" className='w-[70px] h-[70px] z-10 relative p-1 mt-2' />
                         <div className='absolute w-[80px] inset-0 bg-myColor-700 h-[50px] top-5 left-0 rounded-full blur-lg'></div>
                     </div>
                 </div>
