a6b75c45b1583b3a36938a6a1bfcae13a0ce2de8
lckyend <lckyend>
2024-07-28 18:42:24 +0200

    boost

--- backend/routes/game_route.py
+++ backend/routes/game_route.py
@@ -12,6 +12,6 @@ async def get_async(user_id: int, service: PointService = Depends()):
     return await service.get_async(user_id=user_id)
 
 @router.post('/claim')
-async def claim_async(user_id: int, current_path: float, claim: float, service: PointService = Depends()):
-    return await service.claim_async(user_id=user_id, current_path=current_path, claim=claim)
+async def claim_async(user_id: int, current_path: float, coin: float, service: PointService = Depends()):
+    return await service.claim_async(user_id=user_id, current_path=current_path, coin=coin)
 
