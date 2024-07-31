85b5a35292d37e068d7ce204c6a9e4a4b3c1641d
Kwed <159473699+Kwed1@users.noreply.github.com>
2024-07-31 15:38:00 +0400

    try to fix

--- client/web/src/components/services/redirectToGame.jsx
+++ client/web/src/components/services/redirectToGame.jsx
@@ -4,17 +4,37 @@ const RedirectToGame = () => {
   useEffect(() => {
     const redirect = () => {
       if (window.Telegram && window.Telegram.WebApp) {
-        const telegramData = window.Telegram.WebApp.getData();
-        localStorage.setItem('telegramData', JSON.stringify(telegramData));
+        const webApp = window.Telegram.WebApp;
+
+        // Проверьте, существует ли метод initData
+        if (webApp.initData) {
+          try {
+            // Получаем данные из initData
+            const telegramData = webApp.initData;
+            console.log('Telegram Data:', telegramData); // Логирование данных для отладки
+            localStorage.setItem('telegramData', JSON.stringify(telegramData));
+          } catch (error) {
+            console.error('Error retrieving Telegram data:', error);
+          }
+        } else {
+          console.error('The initData is not available on Telegram.WebApp');
+        }
+      } else {
+        console.error('Telegram.WebApp is not available');
       }
 
       const params = new URLSearchParams(window.location.search);
-      const gameUrl = `https://spa.camelracing.io/boost/?${params.toString()}`;
-      console.log(gameUrl);
+      const gameUrl = `https://lifekor.github.io/CamelSwipe/?${params.toString()}`;
+      console.log('Redirecting to:', gameUrl); // Логирование URL для отладки
       window.location.href = gameUrl;
     };
 
-    redirect();
+    // Убедитесь, что SDK загружен и готов к использованию
+    if (window.Telegram && window.Telegram.WebApp) {
+      redirect();
+    } else {
+      console.error('Telegram.WebApp is not initialized');
+    }
   }, []);
 
   return (
